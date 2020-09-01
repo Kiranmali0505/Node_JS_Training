a4175a2de3ec8a4627847cfbf9c4912dd000a602
Kiranmali0505 <69839113+Kiranmali0505@users.noreply.github.com>
2020-09-01 12:53:34 +0530

    Assignment#angular#hrader-footer

---
+++ Assignment#Angular-header-footer/booksweb-client/src/app/books-web-footer.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'app-booksweb-footer',
+   
+    templateUrl:'book-web-footer.component.html'
+})
+export class BooksWebFooterComponent{
+}
